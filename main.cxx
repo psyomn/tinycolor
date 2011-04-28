b8cd7f68483ef6174d696941945695c1b6467c27
psyomn <psyomn@xenagoras.net>
2011-04-27 22:29:04 -0400

    I... forgot the hxx file? Lol. Some untested windows support now.

--- main.cxx
+++ main.cxx
@@ -6,26 +6,36 @@ It is not a versatile solution as some other libraries. It's sole purpose is to
 1) highlight debug info in other projects
 2) Add pretty colors to rogue like games in C++ (or any other cli based game)
 
-\author psyomn
+\author psyomn psyomn@xenagoras.net , lethaljellybean@gmail.com
 */
 #include <iostream>
 #include <string>
 using namespace std;
 
 #include "TinyColor.hxx"
+using namespace tinyc;
 
 int main(){
   TinyColor t;
 
-  t.wc("aaaa\n", tinyc::TC_BLA);
-  t.wc("aaaa\n", tinyc::TC_RED);
-  t.wc("aaaa\n", tinyc::TC_CYA);
-  t.wc("aaaa\n", tinyc::TC_GRY);
-  t.wc("aaaa\n", tinyc::TC_BLU);
-  t.wc("aaaa\n", tinyc::TC_GRE);
-  t.wc("aaaa\n", tinyc::TC_MAG);
-  t.wc("aaaa\n", tinyc::TC_YEL);
-  t.wc("aaaa\n", tinyc::TC_WHI);
+  t.wc("aaaa\n", TC_BLA);
+  t.wc("aaaa\n", TC_RED);
+  t.wc("aaaa\n", TC_CYA);
+  t.wc("aaaa\n", TC_GRY);
+  t.wc("aaaa\n", TC_BLU);
+  t.wc("aaaa\n", TC_GRE);
+  t.wc("aaaa\n", TC_MAG);
+  t.wc("aaaa\n", TC_YEL);
+  t.wc("aaaa\n", TC_WHI);
 
+  // TODO - weird behaviour
+  t.wc("              \n", TC_RED, TC_RED + TC_BACK);
+  t.wc("              \n", 0, TC_NMOD);
+  t.wc("              \n", TC_RED, TC_RED + TC_BACK);
+  t.wc("              \n", 0, TC_NMOD);
+  t.wc("              \n", TC_RED, TC_RED + TC_BACK);
+  t.wc("              \n", 0, TC_NMOD);
   return 0;
 }
+
+
